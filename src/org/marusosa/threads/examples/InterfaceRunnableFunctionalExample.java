b90b7e123bb01a1b097d7a44900c8a83f210bf32
Maru Sosa <soassi@outlook.com>
2022-09-20 15:35:08 -0500

    using Runnable to implement threads

---
+++ src/org/marusosa/threads/examples/InterfaceRunnableFunctionalExample.java
@@ -0,0 +1,29 @@
+package org.marusosa.threads.examples;
+
+import org.marusosa.threads.examples.runnable.TravelTask;
+
+public class InterfaceRunnableFunctionalExample {
+    public static void main(String[] args) {
+
+        Runnable travel = new Runnable() {
+            @Override
+            public void run() {
+                for(int i=0; i<10; i++){
+                    System.out.println(i + " - " + Thread.currentThread().getName());
+                    try {
+                        Thread.sleep((long)(Math.random() * 1000));
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println("I am traveling to " + Thread.currentThread().getName());
+            }
+        };
+
+        new Thread(travel, "Japan").start();
+        new Thread(travel, "United Kingdom").start();
+        new Thread(travel, "Chile").start();
+        new Thread(travel, "Peru").start();
+
+    }
+}

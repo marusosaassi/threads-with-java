aad18b373f14d6942a27ed50714a4127a879e15d
Maru Sosa <soassi@outlook.com>
2022-09-21 12:30:36 -0500

    sleep and join methods

--- src/org/marusosa/threads/examples/InterfaceRunnableFunctionalExample.java
+++ src/org/marusosa/threads/examples/InterfaceRunnableFunctionalExample.java
@@ -3,8 +3,9 @@ package org.marusosa.threads.examples;
 import org.marusosa.threads.examples.runnable.TravelTask;
 
 public class InterfaceRunnableFunctionalExample {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
+        Thread main = Thread.currentThread();
         Runnable travel = () ->
             {
                 for(int i=0; i<10; i++){
@@ -16,12 +17,30 @@ public class InterfaceRunnableFunctionalExample {
                     }
                 }
                 System.out.println("I am traveling to " + Thread.currentThread().getName());
+                System.out.println(main.getState());
             };
 
-        new Thread(travel, "Japan").start();
-        new Thread(travel, "United Kingdom").start();
-        new Thread(travel, "Chile").start();
-        new Thread(travel, "Peru").start();
+        Thread travel1 = new Thread(travel, "Japan");
+        Thread travel2= new Thread(travel, "United Kingdom");
+        Thread travel3 = new Thread(travel, "Chile");
+        Thread travel4 = new Thread(travel, "Peru");
 
+        travel1.start();
+        travel2 .start();
+        travel3.start();
+        travel4.start();
+
+        travel1.join(); //here I join the main thread with the 'travel1' thread
+        travel2 .join();
+        travel3.join();
+        travel4.join();
+
+        //in this way we expect every thread to end to continue the execution of the program
+
+        // Thread.sleep(10000); //this thread.sleep here is a pause in the main, not in the threads
+        // join, in the other way, waits for the other threads that are being executed into the main thread to end
+        System.out.println("Keep executing the main method: " + main.getName());
+        // so,  the treads execute while the main enters a pause method, at the end, when all
+        // the threads end, the main continues its execution
     }
 }

267a8392d7afd9e715153491279a7e0ab5dd3098
Maru Sosa <soassi@outlook.com>
2022-09-22 13:11:03 -0500

    scheduled tasks that repeat themselves in a time interval

---
+++ src/org/marusosa/threads/TimerExample/ScheduleTaskTimerIntervalExample.java
@@ -0,0 +1,41 @@
+package org.marusosa.threads.TimerExample;
+
+import java.awt.*;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ScheduleTaskTimerIntervalExample {
+    public static void main(String[] args) {
+
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        AtomicInteger atomicCounter = new AtomicInteger(3);
+
+        Timer timer = new Timer();
+        /* the Timer class in Java (java.util) lets us add
+        tasks and program them */
+
+        timer.schedule(new TimerTask() {
+            //private int counter = 3;
+            @Override
+            public void run() {
+                int i = atomicCounter.getAndDecrement();
+                if(i > 0) {
+                    toolkit.beep();
+                    System.out.println("Periodic task " + i +" in "
+                            + new Date().toString()
+                            + " Thread name: "
+                            + Thread.currentThread().getName());
+                    //counter--;
+                } else {
+                    System.out.println("Time is over");
+                    timer.cancel(); //comment this to avoid canceling
+                }
+            }
+        }, 0, 10000);
+
+        System.out.println("Schedule a immediate task that repeats itself every 10 seconds ...");
+
+    }
+}
